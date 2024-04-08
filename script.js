27632f8d7e071e02e455e189c118f5aca48ff7d6
nekushi <122607874+nekushi@users.noreply.github.com>
2024-04-08 20:26:26 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let docWidth = window.innerWidth - 20
 let docHeight = window.innerHeight - 20
 
+// create night sky theme background
 function createStar() {
     let starContainer = document.createElement("div")
     let star = document.createElement("div")
@@ -14,7 +15,46 @@ function createStar() {
     starContainer.appendChild(star)
     document.body.appendChild(starContainer)
 }
-
+// loop stars to make more
 for (let i = 0; i < Math.floor(docWidth / 25); i++) {
     createStar()
 }
+
+const menu = document.querySelector(".hamburger-icon")
+const line1 = document.querySelector(".hamburger-icon > .line-1")
+const line2 = document.querySelector(".hamburger-icon > .line-2")
+const line3 = document.querySelector(".hamburger-icon > .line-3")
+const menuList = document.querySelector("nav > ul")
+const main = document.querySelector("main")
+
+let isMenuOpen = false
+function openMenuLines() {
+    line1.style.transform = "rotate(40deg)"
+    line2.style.opacity = "0"
+    line3.style.transform = "rotate(-40deg)"
+    isMenuOpen = true
+}
+function closeMenuLines() {
+    line1.style.transform = "rotate(0)"
+    line2.style.opacity = "1"
+    line3.style.transform = "rotate(0)"
+    isMenuOpen = false
+}
+function showMenuList() {
+    // main.style.marginTop = "-130px"
+    menuList.style.display = "flex"
+}
+function hideMenuList() {
+    // main.style.marginTop = "0"
+    menuList.style.display = "none"
+}
+
+menu.addEventListener("click", () => {
+    if (!isMenuOpen) {
+        openMenuLines()
+        showMenuList()
+    } else {
+        closeMenuLines()
+        hideMenuList()
+    }
+})
